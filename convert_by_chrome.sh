fdeebae5c2b282b6d78d47d3315d2b101098a241
Aikige <48190566+aikige@users.noreply.github.com>
2023-12-31 16:50:36 +0900

    [up] add delay to wait background.

--- convert_by_chrome.sh
+++ convert_by_chrome.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 export PATH=/Applications/Google\ Chrome.app/Contents/MacOS:$PATH
-if [ -n "$PNG" ]; then
+if [[ "$2" =~ _l\.png$ ]]; then
 	# 97x61mm in px.
 	px_size=${PX_SIZE-"367,231"}
 	opt="--screenshot=$2 --window-size=$px_size"
+elif [[ "$2" =~ _p\.png$ ]]; then
+	px_size=${PX_SIZE-"231,367"}
+	opt="--screenshot=$2 --window-size=$px_size"
 fi
 opt=${opt-"--print-to-pdf=$2"}
-Google\ Chrome --headless --disable-gpu $opt $1
+Google\ Chrome --virtual-time-budget=1000 --headless --disable-gpu $opt $1
+#Google\ Chrome --virtual-time-budget=1000 --headless=new $opt $1
